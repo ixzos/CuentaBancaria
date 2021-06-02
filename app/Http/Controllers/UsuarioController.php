04044a1241fd7d4bb3312459e5c2660fb1480c7d
ixzos <lombanayeferson@gmail.com>
2021-06-02 09:09:07 -0500

    Se crean modelos y controladores para las entidades usuario y cuenta, se adicionan las rutas de acceso y creación de endpoints para usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $usuarios = Usuario::all();
+        return $usuarios;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $usuario = new Usuario;
+        $usuario -> nombre = $request -> input('nombre');
+        $usuario -> apellido = $request -> input('apellido');
+        $usuario -> cedula = $request -> input('cedula');       
+        $usuario -> save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($cedula)
+    {
+        //
+        /*$usuarioReturn = Usuario::where('cedula',$cedula)->get('nombre','apellido');*/
+        $usuarioReturn = Usuario::where('cedula', $cedula)->get();
+        
+        return $usuarioReturn;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/usuarios','App\Http\Controllers\UsuarioController');
+Route::resource('/cuentas','App\Http\Controllers\CuentaController');
